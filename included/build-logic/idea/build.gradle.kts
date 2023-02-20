620c9f99e860011424d79ab60444f1edb0e8d733
Stefan Wolf <wolf@gradle.com>
2023-02-20 09:10:11 +0100

    Add custom logic for different IDEA plugins

--- included/build-logic/idea/build.gradle.kts
+++ included/build-logic/idea/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
     `kotlin-dsl`
 }
+
+tasks.register("nestedIdeaTask") {
+    doLast {
+        println("Hello from included:buildLogic:idea")
+    }
+}
--- root/build-logic/idea/build.gradle.kts
+++ root/build-logic/idea/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins {
-    `kotlin-dsl`
+    `groovy-gradle-plugin`
+}
+
+tasks.register("ideaTask") {
+    doLast {
+        println("Hello from buildLogic:idea")
+    }
 }
